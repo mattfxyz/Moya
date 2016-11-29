f45efa09ec45e77f01dc7f9acb2562a2b5d13a34
Matt Faluotico <matt.faluotico@xero.com>
2016-11-29 10:13:33 -0800

    using swift 2.3

--- Source/Moya+Defaults.swift
+++ Source/Moya+Defaults.swift
@@ -3,7 +3,7 @@ import Alamofire
 /// These functions are default mappings to MoyaProvider's properties: endpoints, requests, manager, etc.
 public extension MoyaProvider {
     public final class func DefaultEndpointMapping(target: Target) -> Endpoint<Target> {
-        let url = target.baseURL.URLByAppendingPathComponent(target.path).absoluteString
+        let url = target.baseURL.URLByAppendingPathComponent(target.path)!.absoluteString
         return Endpoint(URL: url, sampleResponseClosure: {.NetworkResponse(200, target.sampleData)}, method: target.method, parameters: target.parameters)
     }
 
